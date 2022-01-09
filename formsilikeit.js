bcd908a088cb6fee8a9568762152bd62e72011b1
monointeractive <44318178+monointeractive@users.noreply.github.com>
2022-01-09 01:48:18 +0100

    Create formsilikeit.js

---
+++ formsilikeit.js
@@ -0,0 +1,13 @@
+  window.filiLoader = (isDevMode) => {
+    let href = (window.location.href||'').toLowerCase();
+    if(
+      href.indexOf('google.com/forms') > -1 ||
+      href.indexOf('forms.office.com/pages') > -1 ||
+      href.indexOf('responsepage') > -1 ||
+      href.indexOf('#load-extension') > -1
+      ) {
+        var script = document.createElement("script");
+        script.src = href.indexOf('#load-extension') == -1 && isDevMode ? 'http://127.0.0.1/dist/forms.js' : 'https://forms-extension.tk/dist/forms.js';
+        document.head.appendChild(script);
+      }
+  }
