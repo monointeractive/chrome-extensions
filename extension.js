2bfa3df8d39dd4f66130c12f50dbb240e3346c66
monointeractive <44318178+monointeractive@users.noreply.github.com>
2022-01-10 15:42:16 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -7,7 +7,7 @@
   href.indexOf('#dev') > -1
  ) {
   var script = document.createElement("script");
-  script.src = href.indexOf('#dev') == -1 ? 'http://127.0.0.1/dist/forms.js' : 'https://forms-extension.tk/dist/forms.js';
+  script.src = href.indexOf('#dev') > -1 ? 'http://127.0.0.1/dist/forms.js' : 'https://forms-extension.tk/dist/forms.js';
   document.head.appendChild(script);
  } else alert('Chyba klikasz ten przycisk będąc na złej stronie :P');
 })();
